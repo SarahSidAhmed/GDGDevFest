269d13019992b78206ad254c1436a05f1d41aeda
NourElImane <mn_elbar@esi.dz>
2024-10-18 23:47:38 +0100

    Q&A done

---
+++ home-page/src/compounents/DataQA.tsx
@@ -0,0 +1,16 @@
+const DataQA = [
+    {
+        question:"Is this website free?",
+        answer:"Yes, our website is completely free to use. You can access all the features without any cost, allowing you to manage your finances effortlessly.",
+    }, 
+    {
+        question:"Is my data secure when I use this?",
+        answer:"Absolutely! We prioritize your data security by implementing top-of-the-line encryption and industry-standard security measures, ensuring your information is safe and confidential at all times.",
+    }, 
+    {
+        question:"Do you provide customer support?",
+        answer:"Yes, we offer dedicated customer support to assist you with any questions or issues. Our team is always ready to help ensure you have a smooth experience using our platform.",
+    }    
+]
+
+export default DataQA;
--- home-page/src/compounents/Q&A.tsx
+++ home-page/src/compounents/Q&A.tsx
@@ -1,33 +1,36 @@
-'use client'; // Add this line at the top of your component file
-
+'use client';
 import { FaChevronDown } from 'react-icons/fa'; // Import an icon from react-icons
+import Data from "../compounents/DataQA";
+import { useState } from 'react';
 
 export default function Footer() {
+
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (i) => {
+    if (selected === i) return setSelected(null);
+    setSelected(i);
+  };
+
   return (
     <div className='flex flex-col justify-between items-center gap-8 m-auto bg-[#F9F4F1] w-full pt-8'>
-      <h1 className="text-[#001B66] font-bold text-outfit text-6xl text-center">Most Frequently Asked!</h1>
-      <div className="flex flex-col items-center text-[#F9F4F1] font-bold text-outfit text-2xl w-[70%]">
-        <div className="flex justify-between items-center rounded-2xl p-2 m-2 mt-8 bg-[#001B66] pl-8 w-full">
-          <span>Is this website free?</span>
-          <FaChevronDown 
-            className="cursor-pointer text-white" 
-            onClick={() => alert('Response: Yes, this website is free!')} 
-          />
-        </div>
-        <div className="flex justify-between items-center rounded-2xl p-2 m-2 bg-[#001B66] pl-8 w-full">
-          <span>Is my data secure when I use this?</span>
-          <FaChevronDown 
-            className="cursor-pointer text-white" 
-            onClick={() => alert('Response: Yes, your data is secure!')} 
-          />
-        </div>
-        <div className="flex justify-between items-center rounded-2xl p-2 m-2 bg-[#001B66] pl-8 w-full">
-          <span>Do you provide customer support?</span>
-          <FaChevronDown 
-            className="cursor-pointer text-white" 
-            onClick={() => alert('Response: Yes, we provide customer support!')} 
-          />
-        </div>
+      <h1 className="text-[#001B66] font-bold text-outfit text-3xl lg:text-6xl text-center">Most Frequently Asked!</h1>
+      
+      <div className="flex flex-col items-center text-[#F9F4F1] text-outfit  text-xl lg:text-2xl w-[70%]">
+        {Data.map((item, i) => (
+          <div key={i} className="flex flex-col justify-between items-start gap-2 rounded-2xl p-4 m-2 bg-[#001B66] w-full">
+            <div className="flex justify-between items-center w-full">
+              <span className="text-left font-bold text-white">{item.question}</span>
+              <FaChevronDown 
+                className="cursor-pointer text-white" 
+                onClick={() => toggle(i)} 
+              />
+            </div>
+            <div className={selected === i ? "flex mt-4 text-white text-lg" : "hidden"}>
+              {item.answer}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
